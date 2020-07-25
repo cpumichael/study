c4648c5030aef55a1e7d0c46ab52cee53183bb51
Michael Barnes <CPU.Michael@gmail.com>
2020-07-25 20:49:57 +0000

    typo

--- merge_sorted.py
+++ merge_sorted.py
@@ -1,11 +1,15 @@
 
 '''
-Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+Given two sorted integer arrays nums1 and nums2, merge nums2
+into nums1 as one sorted array.
 
 Note:
 
-    The number of elements initialized in nums1 and nums2 are m and n respectively.
-    You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+    The number of elements initialized in nums1 and nums2
+    are m and n respectively.
+
+    You may assume that nums1 has enough space (size that
+    is equal to m + n) to hold additional elements from nums2.
 
 Example:
 
@@ -22,7 +26,7 @@ def merge(a, b):
     a_end = len(a)
     b_end = len(b)
 
-    while i < a_end and j < j_end:
+    while i < a_end and j < b_end:
         if a[i] < b[j]:
             ret.append(a[i])
             i += 1
@@ -43,5 +47,12 @@ def merge(a, b):
         j += 1
 
     return ret
+
+if __name__ == '__main__':
+
+    a = [1,2,3,0,0,0]
+    b = [2,5,6]
+
+    print(merge(a, b))
 
 # vim: ai sw=4 ts=4 et showmatch
