11b35682c8a5db04491a2f9a99b8213378a52f47
Michael Barnes <CPU.Michael@gmail.com>
2020-07-28 06:27:02 +0000

    cleanup

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -61,6 +61,7 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 '''
 
 def r2i(r):
+    '''returns an integer representing the given roman numeral r'''
     r = r.upper()
     vals = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     dvals = dict(IV=4, IX=9, XL=40, XC=90, CD=400, CM=900)
