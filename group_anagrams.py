36e3c819350df1c141b8e75400565e358434d438
Michael Barnes <CPU.Michael@gmail.com>
2020-07-28 07:37:41 +0000

    still working

--- group_anagrams.py
+++ group_anagrams.py
@@ -7,21 +7,26 @@ Example:
 Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
 Output:
 [
-  ["ate","eat","tea"],
-  ["nat","tan"],
+  ["ate", "eat", "tea"],
+  ["nat", "tan"],
   ["bat"]
 ]
 
 '''
 
 def group_anagrams(a):
+    ret = []
     i = 0
     while i < len(a):
+        print('i=',i)
         ana = set(a[i])
+        ret.append(a[i])
         print(a[i])
         j = i + 1
         while j < len(a):
             if set(a[j]) == ana:
+                print(ret)
+                #ret[i].append(a[j])
                 print(a[j])
                 del a[j]
             else:
