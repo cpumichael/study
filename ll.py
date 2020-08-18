82886a03e1a1bd9a590d231cd5933736c39fd370
Michael Barnes <CPU.Michael@gmail.com>
2020-08-18 12:39:19 +0000

    small updates

--- ll.py
+++ ll.py
@@ -18,6 +18,7 @@ class LL:
       self.head = None
 
   def append(self, value):
+    '''put value at end of list'''
     if self.head is None:
       self.head = Node(value)
     else:
@@ -33,6 +34,7 @@ class LL:
     self.head = n
 
   def delete(self, value):
+    '''deletes value from list, raises IndexError if not found'''
     prev = None
     curr = self.head
     found = False
@@ -43,13 +45,13 @@ class LL:
       prev = curr
       curr = curr.next
 
-      if not found:
+    if not found:
         raise IndexError('value not in list')
 
-      if prev is None:
-        self.head = curr.next
-      else:
-        prev.next = curr.next
+    if prev is None:
+      self.head = curr.next
+    else:
+      prev.next = curr.next
 
   def show(self):
     print('-' * 10)
